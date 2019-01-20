883b422e333bfe2e20857d99f37fb15636f5332e
Elvin <19018895@qq.com>
2019-01-20 16:05:09 +0800

    demo

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -5,18 +5,19 @@ import {AuthGuardService} from '../core/auth-guard.service';
 
 @Injectable()
 export class DataService {
-  private api_url: string;
+  public data_url: string;
+  public auth_url: string;
   constructor(private http: HttpClient, private auth:AuthGuardService) {
     let lhostparr = location.host.split(":");
     let lhost = lhostparr[0];
-    this.api_url = 'http://' + lhost + '/minierp/common/act.php/';
+    this.data_url = 'http://' + lhost + '/minierp/common/data.php/';
+    this.auth_url = 'http://' + lhost + '/minierp/common/auth.php/';
   }
   async GetData(path:string,par:any) { 
     let userdata = await this.GetUrl(path,par);
     let stat = userdata['stat'];
     //console.log(userdata);
     return userdata;
-
   }
   async GetUrl(dtp:string,opt:any) {
     let token =this.auth.GetToken('');
@@ -27,7 +28,7 @@ export class DataService {
     {
         par += '&' + key + '=' + opt[key];
     }
-    let DataRoot = 'http://' + lhost + '/minierp/common/data.php/' + dtp;
+    let DataRoot = this.data_url + dtp;
     let loadurl = DataRoot + '/?' + par+ '&TOKEN=' + token;;
 
     let data=await this.http.get(loadurl).toPromise();
